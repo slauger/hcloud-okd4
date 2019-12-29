7edd8c306aa59160d0f9ab3effd5f054809e14d7
Simon Lauger <simon@lauger.de>
2019-12-29 22:53:00 +0100

    add loadbalancer pools

---
+++ terraform/pools.tf
@@ -0,0 +1,39 @@
+resource "cloudflare_load_balancer_pool" "bootstrap_pool" {
+  name = "bootstrap-pool"
+
+  dynamic "origins" {
+    for_each = module.bootstrap.server_names
+    content {
+      name    = origins.value
+      address = origins.value
+    }
+  }
+  description = "OpenShift bootstrap node"
+}
+
+resource "cloudflare_load_balancer_pool" "master_pool" {
+  name = "master-pool"
+
+  dynamic "origins" {
+    for_each = module.master.server_names
+    content {
+      name    = origins.value
+      address = origins.value
+    }
+  }
+  description = "OpenShift master nodes"
+}
+
+resource "cloudflare_load_balancer_pool" "worker_pool" {
+  name = "worker-pool"
+
+  dynamic "origins" {
+    for_each = module.worker.server_names
+    content {
+      name    = origins.value
+      address = origins.value
+    }
+  }
+  description = "OpenShift worker nodes"
+}
+
