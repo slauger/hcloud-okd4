bfbc744089715ba925517e185f1ce5da3f122100
Simon Lauger <simon@lauger.de>
2019-12-30 00:15:39 +0100

    select image from data source

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,26 +4,29 @@ module "bootstrap" {
   name           = "master"
   dns_domain     = var.dns_domain
   dns_zone_id    = var.dns_zone_id
-  image          = var.hcloud_image
+  image          = data.hcloud_image.image.id
+  server_type    = "cx41"
   user_data      = file("../ignition/bootstrap.ign")
 }
 
 module "master" {
   source         = "./modules/hcloud_instance"
-  instance_count = 3
+  instance_count = var.replicas_master
   name           = "master"
   dns_domain     = var.dns_domain
   dns_zone_id    = var.dns_zone_id
-  image          = var.hcloud_image
+  image          = data.hcloud_image.image.id
+  server_type    = "cx41"
   user_data      = file("../ignition/master.ign")
 }
 
 module "worker" {
   source         = "./modules/hcloud_instance"
-  instance_count = 3
+  instance_count = var.replicas_worker
   name           = "worker"
   dns_domain     = var.dns_domain
   dns_zone_id    = var.dns_zone_id
-  image          = var.hcloud_image
+  image          = data.hcloud_image.image.id
+  server_type    = "cx41"
   user_data      = file("../ignition/worker.ign")
 }
