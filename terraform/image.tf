9d2a4b8c3029288d2ff738cb9ca7a85b69991f69
Simon Lauger <simon@lauger.de>
2020-03-22 23:08:12 +0100

    add bootstrap_image (for bootstrap node)

--- terraform/image.tf
+++ terraform/image.tf
@@ -1,5 +1,11 @@
 data "hcloud_image" "image" {
-  with_selector = "os=fcos"
+  with_selector = "os=fcos,type=worker"
+  with_status   = ["available"]
+  most_recent   = true
+}
+
+data "hcloud_image" "bootstrap_image" {
+  with_selector = "os=fcos,type=bootstrap"
   with_status   = ["available"]
   most_recent   = true
 }
