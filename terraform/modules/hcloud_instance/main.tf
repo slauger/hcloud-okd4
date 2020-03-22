9155fce5500ac9ce5003fa81ce9ec12491b32866
Simon Lauger <simon@lauger.de>
2020-03-22 23:11:08 +0100

    use fqdn as instance name and disable ipv6 (as ipv6 autoconfig seems to be disabled by default)

--- terraform/modules/hcloud_instance/main.tf
+++ terraform/modules/hcloud_instance/main.tf
@@ -1,6 +1,6 @@
 resource "hcloud_server" "server" {
   count       = var.instance_count
-  name        = format("${var.name}%02d", count.index + 1)
+  name        = "${format("${var.name}%02d", count.index + 1)}.${var.dns_domain}"
   image       = var.image
   server_type = var.server_type
   keep_disk   = var.keep_disk
@@ -22,14 +22,14 @@ resource "cloudflare_record" "dns-a" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "dns-aaaa" {
-  count   = var.instance_count
-  zone_id = var.dns_zone_id
-  name    = element(hcloud_server.server.*.name, count.index)
-  value   = "${element(hcloud_server.server.*.ipv6_address, count.index)}1"
-  type    = "AAAA"
-  ttl     = 1
-}
+#resource "cloudflare_record" "dns-aaaa" {
+#  count   = var.instance_count
+#  zone_id = var.dns_zone_id
+#  name    = element(hcloud_server.server.*.name, count.index)
+#  value   = "${element(hcloud_server.server.*.ipv6_address, count.index)}1"
+#  type    = "AAAA"
+#  ttl     = 1
+#}
 
 resource "hcloud_rdns" "dns-ptr-ipv4" {
   count      = var.instance_count
