af4ce7bf6bb036d306c549d34e2574b61ceb7c81
Simon Lauger <simon@lauger.de>
2020-03-22 23:10:33 +0100

    types for variables.tf

--- terraform/modules/hcloud_instance/variables.tf
+++ terraform/modules/hcloud_instance/variables.tf
@@ -1,8 +1,10 @@
 variable "name" {
+  type        = string
   description = "Instance nam"
 }
 
 variable "dns_domain" {
+  type        = string
   description = "DNS domain"
 }
 
@@ -12,51 +14,60 @@ variable "dns_zone_id" {
 }
 
 variable "instance_count" {
+  type        = number
   description = "Number of instances to deploy"
   default     = 1
 }
 
 variable "server_type" {
+  type        = string
   description = "Hetzner Cloud instance type"
   default     = "cx11"
 }
 
 variable "image" {
+  type        = string
   description = "Hetzner Cloud system image"
   default     = "ubuntu-18.04"
 }
 
 variable "user_data" {
   description = "Cloud-Init user data to use during server creation"
-  default     = "echo provisioned > /var/log/provisioned.log"
+  default     = null
 }
 
 variable "ssh_keys" {
+  type        = list
   description = "SSH key IDs or names which should be injected into the server at creation time"
   default     = []
 }
 
 variable "keep_disk" {
+  type        = bool
   description = "If true, do not upgrade the disk. This allows downgrading the server type later."
   default     = true
 }
 
 variable "location" {
+  type        = string
   description = "The location name to create the server in. nbg1, fsn1 or hel1"
   default     = "nbg1"
 }
 
 variable "backups" {
+  type        = bool
   description = "Enable or disable backups"
   default     = false
 }
 
 variable "volume" {
+  type        = bool
   description = "Enable or disable an additional volume"
   default     = false
 }
 
 variable "volume_size" {
+  type        = number
   description = "Size of the additional data volume"
   default     = 20
 }
