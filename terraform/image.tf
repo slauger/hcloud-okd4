f0b685ea9c61f9f3822bbaa6892fcd5017be358b
Simon Lauger <simon@lauger.de>
2020-04-01 00:51:35 +0200

    remove bootstrap_image

--- terraform/image.tf
+++ terraform/image.tf
@@ -3,9 +3,3 @@ data "hcloud_image" "image" {
   with_status   = ["available"]
   most_recent   = true
 }
-
-data "hcloud_image" "bootstrap_image" {
-  with_selector = "os=fcos,image_type=bootstrap"
-  with_status   = ["available"]
-  most_recent   = true
-}
