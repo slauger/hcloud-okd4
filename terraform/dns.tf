8d0e149b2c133d42b57c37cf8660f6d6ad777fb0
Simon Lauger <simon@lauger.de>
2020-05-24 02:08:19 +0200

    some more fixes

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,37 +1,42 @@
+resource "cloudflare_record" "dns_a_ignition" {
+  zone_id = var.dns_zone_id
+  name    = "ignition.${var.dns_domain}"
+  value   = module.ignition.internal_ipv4_addresses[0]
+  type    = "A"
+  ttl     = 1
+  count   = var.bootstrap == true ? 1 : 0
+}
+
 resource "cloudflare_record" "dns_a_api" {
   zone_id = var.dns_zone_id
   name    = "api.${var.dns_domain}"
-  value   = var.ip_loadbalancer_api
+  value   = var.ip_loadbalancer_api == null ? hcloud_floating_ip.floating_ip.ip_address : var.ip_loadbalancer_api
   type    = "A"
   ttl     = 1
-  count   = var.cloudflare_loadbalancing == false ? 1 : 0
 }
 
 resource "cloudflare_record" "dns_a_api_int" {
   zone_id = var.dns_zone_id
   name    = "api-int.${var.dns_domain}"
-  value   = var.ip_loadbalancer_api_int
+  value   = var.ip_loadbalancer_api_int == null ? hcloud_floating_ip.floating_ip.ip_address : var.ip_loadbalancer_api_int
   type    = "A"
   ttl     = 1
-  count   = var.cloudflare_loadbalancing == false ? 1 : 0
 }
 
 resource "cloudflare_record" "dns_a_apps" {
   zone_id = var.dns_zone_id
   name    = "apps.${var.dns_domain}"
-  value   = var.ip_loadbalancer_apps
+  value   = var.ip_loadbalancer_apps == null ? hcloud_floating_ip.floating_ip.ip_address : var.ip_loadbalancer_apps
   type    = "A"
   ttl     = 1
-  count   = var.cloudflare_loadbalancing == false ? 1 : 0
 }
 
 resource "cloudflare_record" "dns_a_apps_wc" {
   zone_id = var.dns_zone_id
   name    = "*.apps.${var.dns_domain}"
-  value   = var.ip_loadbalancer_apps
+  value   = var.ip_loadbalancer_apps == null ? hcloud_floating_ip.floating_ip.ip_address : var.ip_loadbalancer_apps
   type    = "A"
   ttl     = 1
-  count   = var.cloudflare_loadbalancing == false ? 1 : 0
 }
 
 resource "cloudflare_record" "dns_a_etcd" {
