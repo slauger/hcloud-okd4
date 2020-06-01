aa333c1c774c2e96d19f7cff6527a510689d3a84
Simon Lauger <simon@lauger.de>
2020-06-01 23:55:30 +0200

    fix dns name

--- terraform/modules/hcloud_coreos/dns.tf
+++ terraform/modules/hcloud_coreos/dns.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "dns-a" {
   count   = var.instance_count
   zone_id = var.dns_zone_id
   name    = element(hcloud_server.server.*.name, count.index)
-  value   = var.dns_internal_ip == true ? element(hcloud_server.server.*.ipv4_address, count.index) : element(hcloud_server_network.server_network.*.ip, count.index)
+  value   = var.dns_internal_ip == true ? element(hcloud_server_network.server_network.*.ip, count.index) : element(hcloud_server.server.*.ipv4_address, count.index)
   type    = "A"
   ttl     = 1
 }
