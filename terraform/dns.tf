8ee99556d6eec49f90495f9ae975cbed28c084b5
Simon Lauger <8333300+slauger@users.noreply.github.com>
2020-06-07 14:46:06 +0200

    remove name argument from dns_srv_etcd (#35)
    * remove name argument from dns_srv_etcd
    * remove data block
    * ignore changes on data block

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -52,16 +52,11 @@ resource "cloudflare_record" "dns_a_etcd" {
 resource "cloudflare_record" "dns_srv_etcd" {
   zone_id = var.dns_zone_id
   name    = "_etcd-server-ssl._tcp.${var.dns_domain}"
+  value   = "0\t2380\tetcd-${count.index}.${var.dns_domain}"
   type    = "SRV"
 
-  data = {
-    service  = "_etcd-server-ssl"
-    proto    = "_tcp"
-    name     = "_etcd-server-ssl._tcp.${var.dns_domain}"
-    priority = 0
-    weight   = 0
-    port     = 2380
-    target   = "etcd-${count.index}.${var.dns_domain}"
+  lifecycle {
+    ignore_changes = [data]
   }
 
   count = length(module.master.ipv4_addresses)
