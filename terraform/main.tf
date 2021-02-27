2042c25b651215cf0b0d86bb4a15e0f3de60eca5
Simon Lauger <simon@lauger.de>
2021-02-27 21:48:26 +0100

    fix: remove ocp override for ignition_version

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,6 @@ module "bootstrap" {
   server_type      = "cx41"
   subnet           = hcloud_network_subnet.subnet.id
   ignition_url     = var.bootstrap == true ? "http://${cloudflare_record.dns_a_ignition[0].name}/bootstrap.ign" : ""
-  ignition_version = var.image == "fcos" ? "3.0.0" : "2.2.0"
 }
 
 module "master" {
@@ -42,7 +41,6 @@ module "master" {
   subnet           = hcloud_network_subnet.subnet.id
   ignition_url     = "https://api-int.${var.dns_domain}:22623/config/master"
   ignition_cacert  = local.ignition_master_cacert
-  ignition_version = var.image == "fcos" ? "3.0.0" : "2.2.0"
 }
 
 module "worker" {
@@ -59,5 +57,4 @@ module "worker" {
   subnet           = hcloud_network_subnet.subnet.id
   ignition_url     = "https://api-int.${var.dns_domain}:22623/config/worker"
   ignition_cacert  = local.ignition_worker_cacert
-  ignition_version = var.image == "fcos" ? "3.0.0" : "2.2.0"
 }
