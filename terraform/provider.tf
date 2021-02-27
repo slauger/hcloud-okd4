ed7877b782a9050e6f2ca56a362984d610932227
Simon Lauger <simon@lauger.de>
2021-02-27 21:40:24 +0100

    fix: fix moudle requirements for terraform 0.14.x (required_providers)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,19 +1,14 @@
 provider "cloudflare" {
-  version = "2.14.0"
 }
 
 provider "hcloud" {
-  version = "1.23.0"
 }
 
 provider "template" {
-  version = "2.2.0"
 }
 
 provider "local" {
-  version = "1.4.0"
 }
 
 provider "random" {
-  version = "2.3.1"
 }
---
+++ terraform/versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "2.14.0"
+    }
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.23.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "1.4.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "2.3.1"
+    }
+  }
+  required_version = ">= 0.14"
+}
