6357fd282f8e521409d8362b34e118f0624a4ab8
Simon Lauger <simon@lauger.de>
2021-02-27 21:39:18 +0100

    fix: fix deprecated interpolation-only expressions

--- terraform/modules/hcloud_coreos/output.tf
+++ terraform/modules/hcloud_coreos/output.tf
@@ -1,19 +1,19 @@
 output "server_ids" {
-  value = "${hcloud_server.server.*.id}"
+  value = hcloud_server.server.*.id
 }
 
 output "server_names" {
-  value = "${hcloud_server.server.*.name}"
+  value = hcloud_server.server.*.name
 }
 
 #output "internal_ipv4_addresses" {
-#  value = "${hcloud_server_network.server_network.*.ip}"
+#  value = hcloud_server_network.server_network.*.ip
 #}
 
 output "ipv4_addresses" {
-  value = "${hcloud_server.server.*.ipv4_address}"
+  value = hcloud_server.server.*.ipv4_address
 }
 
 output "ipv6_addresses" {
-  value = "${hcloud_server.server.*.ipv6_address}"
+  value = hcloud_server.server.*.ipv6_address
 }
