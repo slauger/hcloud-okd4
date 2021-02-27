9aec0a6e1750a56097e1ff4b8d2de598ac3551f8
Simon Lauger <simon@lauger.de>
2021-02-27 23:25:56 +0100

    fix: set ttl to 120 seconds

--- terraform/modules/hcloud_coreos/dns.tf
+++ terraform/modules/hcloud_coreos/dns.tf
@@ -5,5 +5,5 @@ resource "cloudflare_record" "dns-a" {
   # value   = var.dns_internal_ip == true ? element(hcloud_server_network.server_network.*.ip, count.index) : element(hcloud_server.server.*.ipv4_address, count.index)
   value = element(hcloud_server.server.*.ipv4_address, count.index)
   type  = "A"
-  ttl   = 1
+  ttl   = 120
 }
