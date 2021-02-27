b0415e03300b815b1551cc351d2c160a8a557b97
Simon Lauger <simon@lauger.de>
2021-02-27 23:27:10 +0100

    fix: add cloudflare resolvers

--- terraform/modules/hcloud_coreos/ignition.tf
+++ terraform/modules/hcloud_coreos/ignition.tf
@@ -3,6 +3,7 @@ data "template_file" "ignition_config" {
   vars = {
     hostname         = "${format("${var.name}%02d", count.index + 1)}.${var.dns_domain}"
     hostname_b64     = base64encode("${format("${var.name}%02d", count.index + 1)}.${var.dns_domain}")
+    resolvconf_b64   = base64encode(file("${path.module}/templates/resolv.conf"))
     ignition_url     = var.ignition_url
     ignition_version = var.ignition_version
     ignition_cacert  = var.ignition_cacert
